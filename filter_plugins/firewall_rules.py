541f38621eba779e038d7fe84c783e7e1873c448
sjonstigter <sjon.stigter@gmail.com>
2026-06-14 22:13:32 +0200

    Update firewall_rules.py

--- filter_plugins/firewall_rules.py
+++ filter_plugins/firewall_rules.py
@@ -5,7 +5,7 @@ def combine_firewall_rules(rules, combine_mode="port"):
         if str(rule.get("rule_match", "")).strip().lower() == "rule matched":
             continue
 
-    for rule in rules:
+#    for rule in rules:
         key_parts = [
             rule.get("description"),
             rule.get("source_zone"),
